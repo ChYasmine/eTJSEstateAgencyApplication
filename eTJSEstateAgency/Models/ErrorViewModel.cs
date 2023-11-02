bfdfaf790dc17f927e46b716cf72a9e33d5af3f6
pc <pc@DESKTOP-97PPMS9>
2023-11-02 09:26:30 +0100

    Ajoutez des fichiers projet.

---
+++ eTJSEstateAgency/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eTJSEstateAgency.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
